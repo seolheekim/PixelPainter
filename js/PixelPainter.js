a6499522f89c616f82e2537e5277b6e7ca9e54fd
Seolhee Kim <seolhee@upspringmedia.com>
2017-06-01 18:03:24 -1000

    almost done

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,33 +1,58 @@
-var colorBox = document.createElement("div");
-colorBox.id = "colorContainer";
-document.getElementById("pixelPainter").appendChild(colorBox);
-
-var createColorBox = document.createElement("div");
-createColorBox.id = "buttonContainer";
-document.getElementById("colorContainer").appendChild(createColorBox);
-
-var eraseButton = document.createElement("button");
-eraseButton.className = "btn btn-2 btn-2i";
-eraseButton.innerHTML = "Erase";
-eraseButton.addEventListener("click", erasePixel);
-document.getElementById("buttonContainer").appendChild(eraseButton);
-
-var clearButton = document.createElement("button");
-clearButton.className = "btn btn-2 btn-2j";
-clearButton.innerHTML = "Clear";
-clearButton.addEventListener("click", clearPixel);
-document.getElementById("buttonContainer").appendChild(clearButton);
-
 // //Starting our grid module patters
 function pixelPainterBox(rows, cols){
 
+    var ppCanvas = document.createElement("div");
+    ppCanvas.id = "pp-canvas";
+    document.getElementById("pixelPainter").appendChild(ppCanvas);
+
+
     var newDiv = document.createElement("div");
     newDiv.id = "canvasContainer";
-    document.getElementById("pixelPainter").appendChild(newDiv)
+    document.getElementById("pp-canvas").appendChild(newDiv)
+
+    var colors = document.createElement("div");
+    colors.id = "colors";
+    document.getElementById("pp-canvas").appendChild(colors);
+
+    var colorBox = document.createElement("div");
+    colorBox.id = "colorContainer";
+    document.getElementById("pp-canvas").appendChild(colorBox);
+
+    var createColorBox = document.createElement("div");
+    createColorBox.id = "buttonContainer";
+    document.getElementById("colorContainer").appendChild(createColorBox);
+
+    var eraseButton = document.createElement("button");
+    eraseButton.className = "btn btn-2 btn-2i";
+    eraseButton.innerHTML = "Erase";
+    eraseButton.addEventListener("click", erasePixel);
+    document.getElementById("buttonContainer").appendChild(eraseButton);
+
+    var clearButton = document.createElement("button");
+    clearButton.className = "btn btn-2 btn-2j";
+    clearButton.innerHTML = "Clear";
+    clearButton.addEventListener("click", clearPixel);
+    document.getElementById("buttonContainer").appendChild(clearButton);
+
+    var saveButton = document.createElement("button");
+    saveButton.className = "btn btn-2 btn-2k";
+    saveButton.innerHTML = "Save";
+    saveButton.addEventListener("click", savePixel);
+    document.getElementById("buttonContainer").appendChild(saveButton);
+    var loadButton = document.createElement("button");
+    loadButton.className = "btn btn-2 btn-2l";
+    loadButton.innerHTML = "Load";
+    loadButton.addEventListener("click", loadPixel);
+    document.getElementById("buttonContainer").appendChild(loadButton);
+
+    var controllerImage = document.createElement("div");
+    controllerImage.id = "controller";
+    document.getElementById("pp-canvas").appendChild(controllerImage);
 
     var grid = document.createElement("table");
     grid.id = "canvas";
     document.getElementById("canvasContainer").appendChild(grid);
+
 
     for(var s = 0; s < rows; s++){
       var tr = document.createElement("tr");
@@ -42,9 +67,8 @@ function pixelPainterBox(rows, cols){
       }
     }
   }
-pixelPainterBox(35,35);
+pixelPainterBox(40,74);
 //end grid module patters
-
 
 var randomColor = null;
 
@@ -53,10 +77,6 @@ function chooseButton() {
 }
 
 //Create Color Box abuttonContainer
-var colors = document.createElement("div");
-colors.id = "colors";
-document.getElementById("colorContainer").appendChild(colors);
-
 var targetColorRow1 = document.createElement("div");
 colors.appendChild(targetColorRow1);
 targetColorRow1.id = "colorColumn1";
@@ -89,21 +109,19 @@ var targetColorRow8 = document.createElement("div");
 colors.appendChild(targetColorRow8);
 targetColorRow8.id = "colorColumn8";
 
-
-
-var colorRow1 = ["#520101", "#840000", "#A60000", "#DC0000", "#FF0000"];
-var colorRow2 = ["#D53C00", "#DB4900", "#ED5F07", "#E68700", "#FFAF00"];
-var colorRow3 = ["#A98600", "#DAB600", "#E9D700", "#F8ED62", "#FFF9AE"];
-var colorRow4 = ["#2F6541", "#50763D", "#5D8748", "#7CA569", "#94BB82"];
-var colorRow5 = ["#00059F", "#0229BF", "#2C2CFF", "#4E91FD", "#BAC2FF"];
-var colorRow6 = ["#660066", "#800080", "#BE29EC", "#D896FF", "#EFBBFF"];
-var colorRow7 = ["#F20B97", "#F453AD", "#EE73C4", "#F29EC8", "#F9BBE6"];
-var colorRow8 = ["#212121", "#3E3E3E", "#5C5C5C", "#787878", "#959595"];
+var colorRow1 = ["#520101", "#A60000", "#FF0000"];
+var colorRow2 = ["#D53C00", "#ED5F07", "#FFAF00"];
+var colorRow3 = ["#F5DC05", "#BAA378", "#835229", "#613916"];
+var colorRow4 = ["#2F6541", "#50763D", "#94BB82", "#75B230"];
+var colorRow5 = ["#00059F", "#4E91FD", "#262F56"];
+var colorRow6 = ["#660066", "#522F50", "#AC4E8C"];
+var colorRow7 = ["#F20B97", "#F24680", "#F05F6E", "#FFC3CE"];
+var colorRow8 = ["#212121", "#3A3A38", "#9E9B96"];
 
 function colorSelectorBox(rowColor, targetColorRow) {
   for(var i = 0; i < rowColor.length; i++){
     var createColorButton = document.createElement("div");
-    createColorButton.className = "colorButton";
+    createColorButton.className = "colorButton shake-slow";
     createColorButton.style.backgroundColor = rowColor[i];
     createColorButton.addEventListener("click", chooseButton);
     targetColorRow.appendChild(createColorButton);
@@ -135,5 +153,25 @@ function clearPixel(){ //make it turn black
 
 function erasePixel(){
   randomColor = "white";
+}
+
+var savedColors = [];
+
+function savePixel(){
+  var saveCell = document.querySelectorAll(".cell");
+  for(var m = 0; m < saveCell.length; m++){
+  console.log(saveCell[m].style.backgroundColor);
+  var currentCellColor = saveCell[m].style.backgroundColor;
+  savedColors.push(currentCellColor);
+  // saveCell[m].style.backgroundColor = "white";
+  }
+}
+
+function loadPixel(){
+  var loadCell = document.querySelectorAll(".cell");
+  for(var f = 0; f < loadCell.length; f++){
+    console.log(loadCell[f]);
+    loadCell[f].style.backgroundColor = savedColors[f];
+  }
 }
 
